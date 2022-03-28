cfe47cd25482dce73c8a9a89dba40a989344abe9
queency972 <steve.bernard972@gmail.com>
2022-03-27 22:35:17 -0500

    First commit ( Struct Author, Post )

---
+++ InstagramCloneSB/InstagramCloneSBApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneSBApp.swift
+//  InstagramCloneSB
+//
+//  Created by Steve Bernard on 27/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneSBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InstagramCloneSB/Post.swift
@@ -0,0 +1,33 @@
+//
+//  Post.swift
+//  InstagramCloneSB
+//
+//  Created by Steve Bernard on 27/03/2022.
+//
+
+import Foundation
+
+struct Post: Identifiable {
+    var id : UUID
+    let author: Author
+    let imageName: String
+    let comment: String
+    let creationDate: Date
+    var like: Int = 0
+    var isBookMarked = false
+    
+    
+    static var all = [
+        Post(id: <#UUID#>, author: Author.all[0], imageName: "p1", comment: "Une maison magnifique", creationDate: Date()),
+        
+        Post(id: <#UUID#>, author: Author.all[1], imageName: "p2", comment: "Super vue de nuit ! #NightShot", creationDate: Date().addingTimeInterval(-6000)),
+        
+        Post(id: <#UUID#>, author: Author.all[2], imageName: "p3", comment: "Un moment de répis au bord de la rivière", creationDate: Date().addingTimeInterval(-12000)),
+        
+        Post(id: <#UUID#>, author: Author.all[3], imageName: "p4", comment: "Pont magnifique", creationDate: Date().addingTimeInterval(-32000)),
+        
+        Post(id: <#UUID#>, author: Author.all[4], imageName: "p5", comment: "Une balade en gondole à venise avec mes amis", creationDate: Date().addingTimeInterval(-52000)),
+        
+        Post(id: <#UUID#>, author: Author.all[5], imageName: "p6", comment: "La mer des caraïbes est sûrement la plus belle mer du monde", creationDate: Date().addingTimeInterval(-76000))
+    ]
+}
